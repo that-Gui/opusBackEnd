31a2207d044ad071e35a47f7d3b8383ed6149503
thatGui <gui@thatgui.com>
2022-03-16 17:57:10 +0000

    pic edit

--- models/Account.model.js
+++ models/Account.model.js
@@ -7,7 +7,7 @@ const accountSchema = new Schema(
     industryType: {type: String, default: ' '},
     timezone: {type: String, default: ' '},
     location: {type: String, default: ' '},
-    avatar: {type: String, default: 'images/default-avatar.png'},
+    avatar: {type: String, default: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fyt3.ggpht.com%2Fa%2FAATXAJxrVU_Dpmuo80kdHdvKkFQd-q0BH1CrwSNlmA%3Ds900-c-k-c0xffffffff-no-rj-mo&f=1&nofb=1'},
     email: {type: String, default: ' '},
     telephone: {type: Number, default: ' '},
     employees: {type: Number, default: ' '},
